1784f5472b4bae5d6b94558fef384caa1e32785c
Huizerd <jessehagenaars@gmail.com>
2018-03-05 14:34:40 +0100

    OpenCV tryout

---
+++ opencv_tryout/edge_detection.py
@@ -0,0 +1,19 @@
+import numpy as np
+import argparse
+import cv2
+ 
+cap = cv2.VideoCapture("/home/huis/GoogleDrive/C&S/AE4317_Autonomous_Flight_of_MAVs/Assignment/Group7/ObstacleVideos/IMG_2186.MOV")
+ 
+while(1):
+  ret, frame = cap.read()
+  gray_vid = cv2.cvtColor(frame, cv2.IMREAD_GRAYSCALE)
+  cv2.imshow('Original',frame)
+  edged_frame = cv2.Canny(frame,100,200)
+  cv2.imshow('Edges',edged_frame)
+
+  k = cv2.waitKey(30) & 0xff
+  if k == 27:
+    break
+
+cap.release()
+cv2.destroyAllWindows()
